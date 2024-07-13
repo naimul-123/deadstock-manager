4bd0c5c4908cff6a7cfc8ecaf8b667e2480d83b7
Naimul Islam <naimuliu@gmail.com>
2024-07-14 04:20:09 +0600

    Some done

--- src/app/projection/page.js
+++ src/app/projection/page.js
@@ -1,15 +1,128 @@
 "use client"
-import React from 'react';
+import React, { useRef } from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { saveAs } from 'file-saver';
 
+import { AlignmentType, Document, Packer, Paragraph, TextRun } from 'docx';
 const Projection = () => {
     const [formdata, setFormData] = useState(null)
+    const contentRef = useRef()
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
         setFormData(data);
+
     }
 
+    const handleKeyDown = (event) => {
+        const { key, keyCode } = event;
+        const isNumpadKey = keyCode >= 96 && keyCode <= 105;
+        if (isNumpadKey) {
+            event.preventDefault();
+            alert('Number pad key is not valid');
+
+        }
+    }
+    const handleSaveToWord = async () => {
+        // const content = contentRef.current.innerHTML;
+        // const converted = htmlDocx.asBlob(content);
+        // saveAs(converted, formdata.proj_for)
+        const doc = new Document({
+            compatibility: {
+                doNotExpandShiftReturn: true,
+
+            },
+            sections: [
+                {
+                    properties: {},
+                    children: [
+                        new Paragraph({
+                            alignment: AlignmentType.JUSTIFIED,
+                            spacing: { after: 50 },
+                            children: [
+                                new TextRun({
+                                    text: `\t${formdata.requisitioner} তারিখের  নোটিং এর প্রেক্ষিতে এ অফিসের ${formdata.proj_for} ব্যবহারের জন্য ${formdata.goods_name} ক্রয়ের লক্ষ্যে ইতিবাচক মতামত প্রদান এবং স্থানীয় বাজারদর যাচাইপূর্বক
+    ${formdata.proj_from} ${formdata.total_price} টাকার একটি ব্যয়প্রাক্কলন প্রস্তুত করেছে (প্রাক্কলনের কপি সংযুক্ত) এবং এতদ্সংক্রান্ত পরবর্তী কার্যক্রম সম্পাদনের জন্য জড়সামগ্রী শাখাকে অনুরোধ জানিয়েছে।`,
+                                    font: {
+                                        name: 'sutonnyOMJ'
+                                    },
+                                    color: '#000000',
+                                    size: 28,
+
+
+                                })
+                            ]
+                        }),
+                        new Paragraph({
+                            alignment: AlignmentType.JUSTIFIED,
+                            spacing: { after: 50 },
+                            children: [
+                                new TextRun({
+                                    text: `\t এমতাবস্থায়, ${formdata.proj_from} কর্তৃক প্রদত্ত    মতামত ও প্রাক্কলনের প্রেক্ষিতে বর্ণিত ক্রয়কার্যক্রমটি সম্পাদনের জন্য নিম্নরূপ ব্যবস্থা গ্রহণ করা যেতে পারে। `,
+                                    font: {
+                                        name: 'sutonnyOMJ'
+                                    },
+                                    color: '#000000',
+                                    size: 28,
+
+
+                                })
+                            ]
+                        }),
+
+                        new Paragraph({
+                            alignment: AlignmentType.JUSTIFIED,
+                            spacing: { after: 50 },
+                            children: [
+                                new TextRun({
+                                    text: `\t\tক. ${formdata.proj_from} কর্তৃক প্রদত্ত ৳${formdata.total_price} টাকার ব্যয়প্রাক্কলনটি অনুমোদন করা যেতে পারে।`,
+                                    font: {
+                                        name: 'sutonnyOMJ'
+                                    },
+                                    color: '#000000',
+                                    size: 28,
+
+                                })
+                            ]
+                        }),
+                        new Paragraph({
+                            alignment: AlignmentType.JUSTIFIED,
+                            spacing: { after: 50 },
+                            children: [
+                                new TextRun({
+                                    text: `\t\tখ. প্রস্তাব “ক” অনুমোদিত হলে পিপিআর এর ৬৯ নং ধারায় বর্ণিত বিধানের আলোকে RFQ মেথডে এমমডিউল সিস্টেমের মাধ্যমে ক্রয় কার্যক্রমটি সম্পাদন করা যেতে পারে।`,
+                                    font: {
+                                        name: 'sutonnyOMJ'
+                                    },
+                                    color: '#000000',
+                                    size: 28,
+
+                                })
+                            ]
+                        }),
+                        new Paragraph({
+                            alignment: AlignmentType.JUSTIFIED,
+                            spacing: { after: 1000 },
+                            children: [
+                                new TextRun({
+                                    text: `\tসদয় অনুমোদনের জন্য উপস্থাপিত।`,
+                                    font: {
+                                        name: 'sutonnyOMJ'
+                                    },
+                                    color: '#000000',
+                                    size: 28
+
+                                })
+                            ]
+                        }),
+                    ]
+                }
+            ]
+        })
+        // this.doc.Settings.addCompatibility().doNotExpandShiftReturn();
+        const blob = await Packer.toBlob(doc)
+        saveAs(blob, formdata.proj_for)
+    }
 
     return (
         <div className=" p-8 bg-base-200 min-h-screen">
@@ -24,7 +137,7 @@ const Projection = () => {
                             <label className="label">
                                 <span className="label-text">যে শাখা থেকে রিকুইজিশন আসছে</span>
                             </label>
-                            <input {...register('requisitioner', { required: true })} type="text" placeholder="শাখা/বিভাগের নাম ও নোটিং তারিখ  লিখুন" className="input input-bordered" />
+                            <input {...register('requisitioner', { required: true })} type="text" placeholder="শাখা/বিভাগের নাম ও নোটিং তারিখ  লিখুন" className="input input-bordered" onKeyDown={handleKeyDown} />
 
                             {errors.proj_from && <p className='text-red-600 font-bold'>{errors.proj_from}</p>}
                         </div>
@@ -57,7 +170,7 @@ const Projection = () => {
                             <label className="label">
                                 <span className="label-text">প্রাক্কলিত মোট মূল্য</span>
                             </label>
-                            <input {...register('total_price', { required: true })} type="text" placeholder="মোট মূল্য লিখুন" className="input input-bordered" />
+                            <input {...register('total_price', { required: true })} type="text" placeholder="মোট মূল্য লিখুন" className="input input-bordered" onKeyDown={handleKeyDown} />
 
                         </div>
                         <div className="form-control mt-6 col-span-full">
@@ -67,19 +180,27 @@ const Projection = () => {
                 </div>
             </div>
 
-            {formdata && <div className='container text-justify'>
-                <p className="indent-10">{`  ${formdata.requisitioner} তারিখের  নোটিং এর প্রেক্ষিতে এ অফিসের ${formdata.proj_for} ব্যবহারের জন্য ${formdata.goods_name} ক্রয়ের লক্ষ্যে ইতিবাচক মতামত প্রদান এবং স্থানীয় বাজারদর যাচাইপূর্বক
+            {formdata && <>
+                <div ref={contentRef} style={{ textAlign: 'justify', fontFamily: 'SutonnyOMJ' }}>
+                    <p style={{ textIndent: '40px' }}>{`  ${formdata.requisitioner} তারিখের  নোটিং এর প্রেক্ষিতে এ অফিসের ${formdata.proj_for} ব্যবহারের জন্য ${formdata.goods_name} ক্রয়ের লক্ষ্যে ইতিবাচক মতামত প্রদান এবং স্থানীয় বাজারদর যাচাইপূর্বক
     ${formdata.proj_from} ${formdata.total_price} টাকার একটি ব্যয়প্রাক্কলন প্রস্তুত করেছে (প্রাক্কলনের কপি সংযুক্ত) এবং এতদ্সংক্রান্ত পরবর্তী কার্যক্রম সম্পাদনের জন্য জড়সামগ্রী শাখাকে অনুরোধ জানিয়েছে।`
-                }</p>
-                <p className="indent-10">{`এমতাবস্থায়, ${formdata.proj_from} কর্তৃক প্রদত্ত মতামত ও প্রাক্কলনের প্রেক্ষিতে বর্ণিত ক্রয়কার্যক্রমটি সম্পাদনের জন্য নিম্নরূপ ব্যবস্থা গ্রহণ করা যেতে পারে। `}
-                    <ol className="ml-10">
-                        <li className="">ক. {formdata.proj_from} কর্তৃক প্রদত্ত ৳{formdata.total_price} টাকার ব্যয়প্রাক্কলনটি অনুমোদন করা যেতে পারে।</li>
-                        <li className="">খ. প্রস্তাব “ক” অনুমোদিত হলে পিপিআর এর ৬৯ নং ধারায় বর্ণিত বিধানের আলোকে RFQ মেথডে এমমডিউল সিস্টেমের মাধ্যমে ক্রয় কার্যক্রমটি সম্পাদন করা যেতে পারে।</li>
-                    </ol>
+                    }</p>
+                    <p style={{ textIndent: '40px' }}>{`এমতাবস্থায়, ${formdata.proj_from} কর্তৃক প্রদত্ত    মতামত ও প্রাক্কলনের প্রেক্ষিতে বর্ণিত ক্রয়কার্যক্রমটি সম্পাদনের জন্য নিম্নরূপ ব্যবস্থা গ্রহণ করা যেতে পারে। `}
+                        <ul style={{ marginLeft: "40px", textIndent: '40px', listStyleType: 'none' }}>
+                            <li style={{ padding: '0', margin: '0' }}>ক. {formdata.proj_from} কর্তৃক প্রদত্ত ৳{formdata.total_price} টাকার ব্যয়প্রাক্কলনটি অনুমোদন করা যেতে পারে।</li>
+                            <li style={{ padding: '0', margin: '0' }}>খ. প্রস্তাব “ক” অনুমোদিত হলে পিপিআর এর ৬৯ নং ধারায় বর্ণিত বিধানের আলোকে RFQ মেথডে এমমডিউল সিস্টেমের মাধ্যমে ক্রয় কার্যক্রমটি সম্পাদন করা যেতে পারে।</li>
+                        </ul>
 
-                </p>
-                <p className="indent-10">সদয় অনুমোদনের জন্য উপস্থাপিত</p>
-            </div>}
+                    </p>
+                    <p style={{ textIndent: '40px' }}>সদয় অনুমোদনের জন্য উপস্থাপিত</p>
+
+                </div>
+                <button className="btn bg-gradient-to-r hover:bg-gradient-to-l text-white from-purple-600 to-violet-500" onClick={handleSaveToWord}>ডাউনলোড করুন</button>
+
+            </>
+
+            }
+
         </div>
     );
 };
